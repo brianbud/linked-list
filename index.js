794a82018d88722965789596ac928fec0425f36a
Brian Bud <hello@brianbud.com>
2023-12-18 20:51:14 -0500

    method to return total num of nodes in list

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
 }
 
 let node1 = new ListNode(2);
 let node2 = new ListNode(5);
 node1.next = node2;
+
+let list = new LinkedList(node1);
+console.log(list.size());
